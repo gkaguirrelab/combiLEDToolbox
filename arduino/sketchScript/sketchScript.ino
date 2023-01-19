c5479a966be57036bb42fedfefcf70ac795a3e2d
ozenc <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2023-01-18 23:51:21 -0500

    A draft sketch attempting nested Arduino communication loops

---
+++ arduino/sketchScript/sketchScript.ino
@@ -0,0 +1,41 @@
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+// setup
+void setup() {
+  Serial.begin(57600);
+}
+
+void loop() {
+  listen();
+  if (stringComplete) {
+    switch (inputString[0]) {      // just get the first letter of the string for now 
+      case 'g':
+        Serial.println("Continuing");
+        break;
+      case 'v':  
+        inputString = "";
+        stringComplete = false;
+        listen();
+        if (stringComplete) {
+          Serial.println(inputString); 
+        }
+    }        
+    inputString = "";
+    stringComplete = false; 
+  }
+}  
+
+void listen() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
