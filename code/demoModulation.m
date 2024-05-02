9df5bc5aeb9dd712eb140a65c99a3e406fe8402d
gkaguirre <gkaguirre@me.com>
2024-05-02 14:25:20 -0400

    Updates to demo

--- code/demoModulation.m
+++ code/demoModulation.m
@@ -17,62 +17,49 @@ photoreceptors = photoreceptorDictionaryHuman('observerAgeInYears',observerAgeIn
 
 % Modulation demos
 modDemos = {...
-    'melPulses', ...
+    'melPulsesShiftedBackground', ...
     'riderStockmanDistortion', ...
-    'SConeDistortion', ...
-    'lightFluxFlicker', ...
-    'slowLminusM' ...
-    'lightFluxShifted'
+    'fastLightFluxFlicker', ...
+    'slowSFlicker' ...
+    'slowLminusMFlicker' ...
     };
 
-% Present the options
-charSet = [97:97+25, 65:65+25];
-fprintf('\nSelect a modDemos:\n')
-for pp=1:length(modDemos)
-    optionName=['\t' char(charSet(pp)) '. ' modDemos{pp} '\n'];
-    fprintf(optionName);
+notDone = true;
+while notDone
+
+    % Present the options
+    charSet = [97:97+25, 65:65+25];
+    fprintf('\nSelect a modulation (return to quit):\n')
+    for pp=1:length(modDemos)
+        optionName=['\t' char(charSet(pp)) '. ' modDemos{pp} '\n'];
+        fprintf(optionName);
+    end
+    choice = input('\nYour choice (return for done): ','s');
+
+    if isempty(choice)
+        notDone = false;
+    else
+
+        % Obtain the mod result
+        choice = int32(choice);
+        idx = find(charSet == choice);
+        modResult = feval(modDemos{idx},obj,photoreceptors,cal);
+
+        % Plot the modulation
+        plotModResult(modResult);
+
+        % Pause then present the modulation. Continue until key press
+        fprintf('Press any key to start, and then stop, the modulation...')
+        pause
+        obj.startModulation;
+        fprintf('modulating...')
+        pause
+        fprintf('done\n')
+        obj.stopModulation;
+        obj.goDark;
+    end
+
 end
-choice = input('\nYour choice (return for done): ','s');
-
-% Obtain the mod result
-if ~isempty(choice)
-    choice = int32(choice);
-    idx = find(charSet == choice);
-    modResult = feval(modDemos{idx},obj,photoreceptors,cal);
-end
-
-% Plot the modulation
-plotModResult(modResult);
-
-% Pause and do a couple of cycles of presenting the modulation
-obj.goDark;
-pause
-obj.startModulation;
-pause
-obj.stopModulation;
-obj.goDark;
-pause
-obj.startModulation;
-pause
-obj.stopModulation;
-obj.goDark;
-pause
-obj.startModulation;
-pause
-obj.goDark;
-pause
-obj.startModulation;
-pause
-obj.stopModulation;
-obj.goDark;
-pause
-obj.startModulation;
-pause
-obj.stopModulation;
-obj.goDark;
-pause
-obj.startModulation;
-pause
 
 % Clean up
 obj.serialClose;
@@ -82,18 +69,19 @@ clear
 
 
 
-function modResult = melPulses(obj,photoreceptors,cal)
-modResult = designModulation('Mel_shiftBackground',photoreceptors,cal);
+function modResult = melPulsesShiftedBackground(obj,photoreceptors,cal)
+modResult = designModulation('Mel',photoreceptors,cal,'searchBackground',true,'primariesToMaximize',[3,4],'primaryHeadRoom',0.05);
 obj.setSettings(modResult);
 obj.setUnimodal();
 obj.setWaveformIndex(2); % square-wave
 obj.setFrequency(0.1);
+obj.setPhaseOffset(pi);
 obj.setAMIndex(2); % half-cosine windowing
 obj.setAMFrequency(0.1);
 obj.setAMValues([0.5,0]); % 0.5 second half-cosine on; second value unused
 end
 
-function modResult = lightFluxFlicker(obj,photoreceptors,cal)
+function modResult = fastLightFluxFlicker(obj,photoreceptors,cal)
 modResult = designModulation('LightFlux',photoreceptors,cal);
 obj.setSettings(modResult);
 obj.setWaveformIndex(1);
@@ -101,15 +89,6 @@ obj.setFrequency(16);
 obj.setContrast(1);
 obj.setAMIndex(1);
 obj.setAMFrequency(0.1);
-end
-
-function modResult = SConeDistortion(obj,photoreceptors,cal)
-modResult = designModulation('S_foveal',photoreceptors,cal);
-obj.setSettings(modResult);
-obj.setWaveformIndex(1);
-obj.setFrequency(30);
-obj.setAMIndex(1);
-obj.setAMFrequency(1);
 end
 
 function modResult = riderStockmanDistortion(obj,photoreceptors,cal)
@@ -129,27 +108,19 @@ compoundPhases=deg2rad([0,333,226,0,0]); % Should look red
 obj.setCompoundModulation(compoundHarmonics,compoundAmplitudes,compoundPhases);
 end
 
-function modResult = slowLminusM(obj,photoreceptors,cal)
+function modResult = slowLminusMFlicker(obj,photoreceptors,cal)
 modResult = designModulation('LminusM_wide',photoreceptors,cal);
 obj.setSettings(modResult);
 obj.setWaveformIndex(1);
-obj.setFrequency(1);
+obj.setFrequency(2);
 obj.setAMIndex(0);
 end
 
-function modResult = lightFluxShifted(obj,photoreceptors,cal)
-backgroundPrimary = [0.5222    0.4528    0.1896    0.5264    0.2302    0.3250    0.4875    0.4882]';
 
-modResult = designModulation('LightFlux',photoreceptors,cal,...
-    'searchBackground',false,...
-    'contrastMatchConstraint',4,...
-    'backgroundPrimary',backgroundPrimary);
+function modResult = slowSFlicker(obj,photoreceptors,cal)
+modResult = designModulation('S_wide',photoreceptors,cal);
 obj.setSettings(modResult);
 obj.setWaveformIndex(1);
-obj.setAMIndex(2); % half-cosine windowing
-obj.setAMFrequency(1/24);
-obj.setAMValues([1.5,0]); % duration half-cosine ramp; second value unused
-obj.setDuration(15);
-obj.setFrequency(4);
+obj.setFrequency(0.5);
+obj.setAMIndex(0);
 end
-
