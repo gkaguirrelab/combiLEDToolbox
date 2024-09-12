73e92d241ff8b7a24c11d485e8ff26e66d4d86bb
gkaguirre <gkaguirre@me.com>
2024-09-12 11:34:13 -0400

    Update cal file handling after the calibration

--- code/calibration/@CombiLEDcalibrator/plotBasicMeasurements.m
+++ code/calibration/@CombiLEDcalibrator/plotBasicMeasurements.m
@@ -1,13 +1,28 @@
 % Method that puts up a plot of the essential data
 function plotBasicMeasurements(obj)
-    cal = obj.cal;
-    
-    close all
-    
+cal = obj.cal;
+
+close all
+
+% One plotting approach for if we have a single measurement for a
+% unified primary
+if obj.cal.describe.nMeas == 1 && obj.displayPrimariesNum == 1
+    figure(1); clf;
+    plot(SToWls(cal.rawData.S), cal.rawData.gammaCurveMeanMeasurements ,'-r');
+    hold on
+    plot(SToWls(cal.rawData.S), cal.rawData.ambientMeasurements ,'-k');
+    xlabel('Wavelength (nm)', 'Fontweight', 'bold');
+    ylabel('Power', 'Fontweight', 'bold');
+    title('Phosphor spectra', 'Fontsize', 13, 'Fontname', 'helvetica', 'Fontweight', 'bold');
+    axis([380, 780, -Inf, Inf]);
+    drawnow;
+
+else
+
     figure(1); clf;
     hold on
     for ii=1:size(cal.processedData.P_device,2)
-    plot(SToWls(cal.rawData.S), cal.processedData.P_device(:,ii),'-');
+        plot(SToWls(cal.rawData.S), cal.processedData.P_device(:,ii),'-');
     end
     xlabel('Wavelength (nm)', 'Fontweight', 'bold');
     ylabel('Power', 'Fontweight', 'bold');
@@ -18,15 +33,19 @@ function plotBasicMeasurements(obj)
     figure(2); clf;
     hold on
     for ii=1:size(cal.processedData.P_device,2)
-    plot(cal.rawData.gammaInput, cal.rawData.gammaTable(:,ii), '+');
+        plot(cal.rawData.gammaInput, cal.rawData.gammaTable(:,ii), '+');
     end
     xlabel('Input value', 'Fontweight', 'bold');
     ylabel('Normalized output', 'Fontweight', 'bold');
     title('Gamma functions', 'Fontsize', 13, 'Fontname', 'helvetica', 'Fontweight', 'bold');
     for ii=1:size(cal.processedData.P_device,2)
-    plot(cal.processedData.gammaInput, cal.processedData.gammaTable(:,ii), '-');
+        plot(cal.processedData.gammaInput, cal.processedData.gammaTable(:,ii), '-');
     end
-    
+
     hold off
     drawnow;
-end
+
+end % global if
+
+
+end % function
--- code/calibration/@CombiLEDcalibrator/processRawData.m
+++ code/calibration/@CombiLEDcalibrator/processRawData.m
@@ -2,13 +2,17 @@ function obj = processRawData(obj)
     % reset processData property
     obj.processedData = [];
     
-    % Fitting a linear model to the raw data
-    fprintf('Computing linear model.\n');
-    obj.fitLinearModel();
+    if obj.cal.describe.nMeas > 1
 
-    % Fitting a curve through the raw data
-    fprintf('Fitting raw gamma data.\n');
-    obj.fitRawGamma(2^12);
+        % Fitting a linear model to the raw data
+        fprintf('Computing linear model.\n');
+        obj.fitLinearModel();
+
+        % Fitting a curve through the raw data
+        fprintf('Fitting raw gamma data.\n');
+        obj.fitRawGamma(2^12);
+
+    end
     
     % Process the ambient data
     obj.addAmbientData();
