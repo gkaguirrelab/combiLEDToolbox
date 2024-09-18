c6a61739bca8c4e6eda23cea36cf5697ee2784f5
gkaguirre <gkaguirre@me.com>
2024-09-18 16:26:29 -0400

    Update the direct mode demo

--- code/demoDirectControl.m
+++ code/demoDirectControl.m
@@ -2,8 +2,19 @@
 % Open a CombiLEDcontrol object
 obj = CombiLEDcontrol();
 
+% By default, gamma correction is not performed in directMode. Here we turn
+% gamma correction on.
+obj.setDirectModeGamma(true);
+
+% A default gamma table will be used. One could load a cal file and send 
+% the measured gamma table using the commands below:
+%{
+    cal = selectCal();
+    obj.setGamma(cal.processedData.gammaTable);
+%}
+
 % Device settings are given as a vector of floats in the range of 0-1.
-mySettings = [1,0,0,0,0,0,0,0];
+mySettings = [0.5,0,0,0,0,0,0,0];
 
 % Send the settings
 obj.setPrimaries(mySettings);
