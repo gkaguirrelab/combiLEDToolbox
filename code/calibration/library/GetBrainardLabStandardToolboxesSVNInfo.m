1478d2ba0c7ee24c8aec8384c093fb7cb162edf6
gkaguirre <gkaguirre@me.com>
2025-02-27 12:55:09 -0500

    Take an empty input argument for code compatability.

--- code/calibration/library/GetBrainardLabStandardToolboxesSVNInfo.m
+++ code/calibration/library/GetBrainardLabStandardToolboxesSVNInfo.m
@@ -1,4 +1,4 @@
-function info = GetBrainardLabStandardToolboxesSVNInfo
+function info = GetBrainardLabStandardToolboxesSVNInfo(~)
 % The calibration code has a required call to this function, which we
 % shadow here so we don't need to deal with this legacy SVN system
 
