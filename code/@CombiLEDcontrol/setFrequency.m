d1dc515ac04c8f7c79ba37b9aa6c52d1bf14b86c
gkaguirre <gkaguirre@me.com>
2025-03-25 13:55:33 -0400

    Boundary check the passed frequency value

--- code/@CombiLEDcontrol/setFrequency.m
+++ code/@CombiLEDcontrol/setFrequency.m
@@ -1,10 +1,14 @@
-function setFrequency(obj,frequency)
+function setFrequency(obj,frequencyHz)
 
 % Check that we have an open connection
 if isempty(obj.serialObj)
     warning('Serial connection not yet established');
     return
 end
+
+% Check that we have a valid frequency value. Note that a value of zero is
+% invalid and will cause strange behavior from the combiLED firmware.
+assert(frequencyHz > 0);
 
 % Place the CombiLED in Config Mode
 switch obj.deviceState
@@ -20,7 +24,7 @@ writeline(obj.serialObj,'FQ');
 readline(obj.serialObj);
 
 % Send the frequency
-writeline(obj.serialObj,num2str(frequency,'%.4f'));
+writeline(obj.serialObj,num2str(frequencyHz,'%.4f'));
 msg = readline(obj.serialObj);
 
 if obj.verbose
