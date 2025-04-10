17bdd1f97d23ce972be1badb27821e29c4e2cfc5
gkaguirre <gkaguirre@me.com>
2025-04-10 12:08:07 -0400

    bug fix?

--- code/silentSubstitution/library/loadCalByName.m
+++ code/silentSubstitution/library/loadCalByName.m
@@ -4,19 +4,19 @@ if nargin == 1
     calSubDir = [];
 end
 
+if nargin == 2
+    calIdx = [];
+end
+
 % Find the specified calibration within this project directory; load
 % it.
 calPath = fullfile(getpref('combiLEDToolbox','CalDataFolder'),calSubDir,calName);
 load(calPath,'cals');
 
 % If not otherwise specified, use the most recent calibration
-if nargin<3
-    if isempty(calIdx)
-        cal = cals{end};
-    else
-        cal = cals{calIdx};
-    end
-else
+if isempty(calIdx)
     cal = cals{end};
+else
+    cal = cals{calIdx};
 end
 end
