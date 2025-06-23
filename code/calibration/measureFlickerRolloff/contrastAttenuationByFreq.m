92ab3ac38848403df455f99fa560bba040040197
gkaguirre <gkaguirre@me.com>
2025-06-23 17:05:52 -0400

    Updated to reflect measurements made with the flicker-BT device

--- code/calibration/measureFlickerRolloff/contrastAttenuationByFreq.m
+++ code/calibration/measureFlickerRolloff/contrastAttenuationByFreq.m
@@ -1,57 +1,39 @@
 function contrastScale = contrastAttenuationByFreq(frequencies)
-% Measurements made using the Klein ChromaSurf software by GKA
-% on April 29, 2025. CombiLED C (A10L31XJ) was running the Arduino
-% firmware committed to GitHub on this day. A 0.5 contrast, light
-% flux sinusoidal modulation was presented at frequencies between
-% 10 and 100 Hz. The "percent" amplitude of response as reported
-% by the Klein software was recorded. The measurement at 10 Hz was
-% 0.4840 contrast (reported by the Klein software as 96.8%). This
-% small deviation from the 0.5 source could be due to ambient light
-% intrusion in the measure. The set of amplitude responses across
-% frequencies were scaled by this 10 Hz value to result in the 
-% relative decrease in modulation depth as a function of modulation
-% frequency.
+% The expected contrast roll-off of the combiLED as a function of frequency
+%
+% Syntax:
+%   contrastScale = contrastAttenuationByFreq(frequencies)
+%
+% Description:
+%   On June 23, 2025, we (GKA and Sophia Miarabal) made measurements of the
+%   sinusoidal flicker output of the CombiLED-A device. The light from the
+%   combiLED was passed to an integrating sphere, and measured using the
+%   Flicker-BT device from labSphere. We measured 2 second increments at
+%   5000 Hz across a range of frequencies. We fit the time series data for
+%   each source frequency using a Fourier regression model and extracted
+%   the amplitude of response. The resulting temporal transfer function was
+%   then modeled as the output of an ideal light source with a fixed,
+%   discrete refresh rate. We found that an ideal device with a refresh
+%   rate of 209.88 Hz provided an excellent description of the data.
+%
+%   This routine models the contrast attentuation by frequency by making
+%   use of the idealDiscreteSampleFilter function
+%
+% Inputs:
+%  frequencies            - Vector of frequencies in Hz.
+%
+% Optional key/value pairs:
+%   none
+%
+% Outputs:
+%   contrastScale         - Scalar. The contast attenuation for the
+%                           specified frequencies.
 
-frequencySupport = [
-   0.1
-   1.0000
-   10.0000
-   15.0000
-   20.0000
-   25.0000
-   30.0000
-   35.0000
-   40.0000
-   50.0000
-   60.0000
-   65.0000
-   70.0000
-   80.0000
-   90.0000
-  100.0000];
 
-contrastRollOff = [
-    1.0000
-    1.0000
-    1.0000
-    1.0000
-    0.9845
-    0.9731
-    0.9610
-    0.9473
-    0.9378
-    0.9181
-    0.9029
-    0.8678
-    0.8394
-    0.7845
-    0.7389
-    0.6839];
+% The observed sampling rate of combiLED-A
+discreteT = 1/209.88;
 
-% Sanity check the input. We can't extrapolate above highest measurement
-mustBeInRange(frequencies,1e-6,max(frequencySupport));
-
-% Interpolate from the data to the contrast values
-contrastScale = interp1(log10(frequencySupport),contrastRollOff,log10(frequencies),'linear',1);
+% The contrast for a discretely sampled source
+contrastScale = idealDiscreteSampleFilter(frequencies,discreteT);
 
 end
